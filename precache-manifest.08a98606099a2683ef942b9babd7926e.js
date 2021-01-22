271d1e7bbd8f96f9916b8d0b1a817d7b24f78287
unknown <badagadzeb@gmail.com>
2021-01-22 23:27:52 +0400

    Updates

---
+++ precache-manifest.08a98606099a2683ef942b9babd7926e.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6a9b41e1c67875fe908020b5d3b00c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0bb02b907dfdc9b11a0e",
+    "url": "./static/css/2.7ad2490a.chunk.css"
+  },
+  {
+    "revision": "68636ed28b78eb3dfbb6",
+    "url": "./static/css/main.9138fc5b.chunk.css"
+  },
+  {
+    "revision": "0bb02b907dfdc9b11a0e",
+    "url": "./static/js/2.859abaf9.chunk.js"
+  },
+  {
+    "revision": "d1b601bb62b1c72271406e4ef4806c0a",
+    "url": "./static/js/2.859abaf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13324d5bbc9924e31488",
+    "url": "./static/js/3.59b9fff5.chunk.js"
+  },
+  {
+    "revision": "3ab159b450a80cd70e3c",
+    "url": "./static/js/4.d64bf318.chunk.js"
+  },
+  {
+    "revision": "24469eab1d2024c9ee7b",
+    "url": "./static/js/5.d21fc716.chunk.js"
+  },
+  {
+    "revision": "c90655395d256c538ca1",
+    "url": "./static/js/6.2d1cbd9c.chunk.js"
+  },
+  {
+    "revision": "68636ed28b78eb3dfbb6",
+    "url": "./static/js/main.4bfc9490.chunk.js"
+  },
+  {
+    "revision": "44a438c47aa40670acb4",
+    "url": "./static/js/runtime-main.ee51cc5b.js"
+  },
+  {
+    "revision": "498e3777cca99c0baf7bf71f6c259727",
+    "url": "./static/media/Arrow-Icon.498e3777.svg"
+  },
+  {
+    "revision": "328d557958b18b54b3bddb3a4a36215a",
+    "url": "./static/media/BPG_DedaEna_NonBlock.328d5579.ttf"
+  },
+  {
+    "revision": "6daa93fb0bd28d8470da0d0d01b75918",
+    "url": "./static/media/MasterC.6daa93fb.svg"
+  },
+  {
+    "revision": "7ac043ce58c2e61adcf7ebac9625d31b",
+    "url": "./static/media/Myriad_Pro_Regular.7ac043ce.ttf"
+  },
+  {
+    "revision": "0c90aede0c3563afb4a21d24c74587f7",
+    "url": "./static/media/Twitter-Icon.0c90aede.svg"
+  },
+  {
+    "revision": "ac37cf6eecadba915954d1b5415579c4",
+    "url": "./static/media/VisaC.ac37cf6e.png"
+  },
+  {
+    "revision": "fb043faec15a23056547c4869b72ec8d",
+    "url": "./static/media/android.fb043fae.svg"
+  },
+  {
+    "revision": "e70283f3b442573da9a6c13b5bb4c0a7",
+    "url": "./static/media/asset1.e70283f3.svg"
+  },
+  {
+    "revision": "166eb985bc249044daf34bd230c99b4f",
+    "url": "./static/media/auth.166eb985.png"
+  },
+  {
+    "revision": "07b735e5863e37041237b30726f251ac",
+    "url": "./static/media/bill.07b735e5.svg"
+  },
+  {
+    "revision": "cb631b17803b90fdd2a34d08a9f4301b",
+    "url": "./static/media/bitcoin-logo.cb631b17.svg"
+  },
+  {
+    "revision": "f2ac90b25649b91f03524e35c7c113fe",
+    "url": "./static/media/bitcoin-logo2.f2ac90b2.svg"
+  },
+  {
+    "revision": "e2a406812422ff25fca81195bd77205d",
+    "url": "./static/media/btc2.e2a40681.svg"
+  },
+  {
+    "revision": "763cf69ce445010134580d284f0dc19a",
+    "url": "./static/media/btc_yellow.763cf69c.svg"
+  },
+  {
+    "revision": "46ff2a7ccf1d550d66dec0b8a9a93c89",
+    "url": "./static/media/cexioIcons.46ff2a7c.svg"
+  },
+  {
+    "revision": "e1443d173cca747e17492790ede7eaa0",
+    "url": "./static/media/creditCard.e1443d17.png"
+  },
+  {
+    "revision": "eeab43614adf397aaecdf129e10bea09",
+    "url": "./static/media/euro.eeab4361.svg"
+  },
+  {
+    "revision": "a07042bd7fe95f529fce93cbfb10ac4b",
+    "url": "./static/media/euroN.a07042bd.svg"
+  },
+  {
+    "revision": "af15893b7beeffa4a2bd66510bda645c",
+    "url": "./static/media/euroN2.af15893b.svg"
+  },
+  {
+    "revision": "57a6318b63cdb2aeab829ff4e42c8055",
+    "url": "./static/media/iPhoneXS.57a6318b.png"
+  },
+  {
+    "revision": "a7908d6d5c05ca88260966b411c08052",
+    "url": "./static/media/iconfinder.a7908d6d.png"
+  },
+  {
+    "revision": "29c0bd5b0b1d89374b0ea51ae0c6ce39",
+    "url": "./static/media/iconfinder_icon-21-eye-hidden_314276.29c0bd5b.svg"
+  },
+  {
+    "revision": "98dc90bc24024d59f6ad6909bffc1ada",
+    "url": "./static/media/iconfinder_icon-22-eye_314277.98dc90bc.svg"
+  },
+  {
+    "revision": "02ad1d2aa15e2a6196da9403a78a27ef",
+    "url": "./static/media/international.02ad1d2a.svg"
+  },
+  {
+    "revision": "b898083aae9197209c502fbca565c026",
+    "url": "./static/media/iphone.b898083a.svg"
+  },
+  {
+    "revision": "34c7f8324c721962575d01166fc5cef7",
+    "url": "./static/media/live.34c7f832.svg"
+  },
+  {
+    "revision": "ee75fbb51d0d94269ce536e97334ca98",
+    "url": "./static/media/message2.ee75fbb5.svg"
+  },
+  {
+    "revision": "512d991061b49b72d70c0f11e1820b65",
+    "url": "./static/media/money.512d9910.svg"
+  },
+  {
+    "revision": "098ec2764c25ce56a826b290862a997b",
+    "url": "./static/media/usd2.098ec276.svg"
+  },
+  {
+    "revision": "cf0d19c1b65b072cccc6e2cb4037f429",
+    "url": "./static/media/verificationLock.cf0d19c1.svg"
+  },
+  {
+    "revision": "5a9a039da800874dff1a9abedea5895b",
+    "url": "./static/media/world2.5a9a039d.png"
+  }
+]);
